b75794bdec0a5f0d5a1ca1f65a2aa3ccc7347c06
Israelistic <haguyle@gmail.com>
2018-07-20 19:09:49 -0400

    added AJAX call to dispay resulats

--- election.js
+++ election.js
@@ -1,5 +1,44 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  // Imagination!
+var finalvotesButton = document.getElementById('finalVotes');
+var uList = document.getElementById('ul');
+var listName = document.createElement('li');
+
+
+
+
+ finalvotesButton.addEventListener('click', function() {
+      $.ajax({
+          url:'https://bb-election-api.herokuapp.com',
+          method:'GET',
+     }).done(function(responseData) {
+          responseData.candidates.forEach(function(candidate) {
+               var candidateName = document.createElement('h2');
+               var candidateVotesResults = document.createElement('h3');
+               candidateName.innerHTML = candidate.name;
+               candidateVotesResults.innerHTML = ' Final Results: ' + candidate.votes
+               listName.append(candidateName)
+               listName.append(candidateVotesResults)
+
+
+          })
+
+          uList.append(listName);
+          
+     }).fail( function(responseData) {
+                  console.log("ERROR");
+                  result.innerHTML = "";
+                  var error = document.createElement('h2');
+                  error.innerHTML = "Whoops, something went wrong...";
+                  result.appendChild(error);
+               });
+
+
+})
+
+
+ // })
+
+
 
 });
